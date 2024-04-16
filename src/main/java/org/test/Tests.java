c5cae14902f4fdbacb1a606cc8eaa1758fbbfab2
serhii.dudar <serhii.dudar@globallogic.com>
2024-04-16 11:02:19 +0300

    updated resume template

--- src/main/java/org/test/Tests.java
+++ src/main/java/org/test/Tests.java
@@ -1,8 +1,31 @@
 package org.test;
 
+import com.google.common.base.Splitter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import org.example.utils.JsonUtil;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
 public class Tests {
 
+    @SneakyThrows
     public static void main(String[] args) {
+
+        List<Data> data = Arrays.asList(
+            JsonUtil.OBJECT_MAPPER.reader().forType(Data.class.arrayType()).readValue("[{\"id\": \"tr\"}, {\"id\": \"tr\"}]"));
+
         System.out.println("someghing test");
+    }
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @lombok.Data
+    public static class Data {
+        private String id;
     }
 }
