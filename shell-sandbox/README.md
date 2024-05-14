ca21d84111fab898253504723404a69445fec915
serhii.dudar <serhii.dudar@globallogic.com>
2024-05-14 19:55:17 +0300

    shell start

--- shell-sandbox/README.md
+++ shell-sandbox/README.md
@@ -1,3 +1,27 @@
 https://www.tutorialspoint.com/unix/index.htm
 https://www.learnshell.org/
 https://www.javatpoint.com/bash
+
+https://habr.com/ru/articles/583320/
+vscode plugins:
+
+dependencies:
+- brew install bashdb
+- brew install hadolint
+
+plugins:
+- ext install timonwong.shellcheck
+- ext install rogalmic.bash-debug
+- ext install foxundermoon.shell-format
+-??? ext install mads-hartmann.bash-ide-vscode
+- ext install tetradresearch.vscode-h2o
+- ext install meronz.manpages
+- ext install Remisa.shellman
+- ext install formulahendry.code-runner
+- ext install exiasr.hadolint
+- ext install jeff-hykin.better-dockerfile-syntax
+- ??? ext install xshrim.txt-syntax
+- ??? ext install jeff-hykin.better-shellscript-syntax
+
+all plugins can be installed by:
+- ext install woozy-masta.shell-script-ide
--- shell-sandbox/tests.sh
+++ shell-sandbox/tests.sh
@@ -1,10 +1,52 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
+export TERM=alacritty
 
-echo hello shell
-alias data=''
+# Levels of indirection and time
+PS4='+\011\[\e[3;34m\]\t\[\e[0m\]'
+# User ID [Effective user ID]: Groups of user is a member
+PS4+=' \[\e[0;35m\]$UID[$EUID]:$GROUPS\[\e[0m\] '
+# Shell level and subshell
+PS4+='\011\[\e[1;31m\]L$SHLVL:S$BASH_SUBSHELL\[\e[0m\]'
+# Source file
+PS4+=' \[\e[1;33m\]${BASH_SOURCE:-$0}\[\e[0m\]'
+# Line number
+PS4+='\[\e[0;36m\]#:${LINENO}\[\e[0m\]'
+# Function name
+PS4+='\011\[\e[1;32m\]${FUNCNAME[0]:+${FUNCNAME[0]}(): }\[\e[0m\]'
+# Executed command
+PS4+='\n# '
+export PS4
 
-function test() {
-    echo "123 $1"
+set -eu
+
+function print-msg() {
+  printf '%b%-20s%b' "${colors[${1:-0}]}" "${@:2}" "${colors[0]}"
 }
 
-test 10
+function random-color-echo() {
+  print-msg $((1 + RANDOM % $((4 - 1)))) "${*:-}"
+}
+
+function msg() {
+  random-color-echo "Hi ${*:-}!"
+}
+
+colors=(
+  "$(tput sgr0)"    # reset
+  "$(tput setaf 1)" # red
+  "$(tput setaf 2)" # green
+  "$(tput setaf 3)" # yellow
+  "$(tput setaf 4)" # blue
+)
+
+for item in {"Bob","Alice"}; do
+  echo "$(
+    {
+      msg "$item"
+      (date '+%s%N')
+    } &
+    wait
+  )"
+done
+
+echo 'Done'
