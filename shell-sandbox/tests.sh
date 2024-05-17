d13fbfce25ed4176c1ca75d1b29139b8acc87d32
serhii.dudar <serhii.dudar@globallogic.com>
2024-05-17 13:10:29 +0300

    bash, cv update

--- shell-sandbox/tests.sh
+++ shell-sandbox/tests.sh
@@ -1,52 +1,36 @@
-#!/usr/bin/env bash
-export TERM=alacritty
+#!/bin/zsh
 
-# Levels of indirection and time
-PS4='+\011\[\e[3;34m\]\t\[\e[0m\]'
-# User ID [Effective user ID]: Groups of user is a member
-PS4+=' \[\e[0;35m\]$UID[$EUID]:$GROUPS\[\e[0m\] '
-# Shell level and subshell
-PS4+='\011\[\e[1;31m\]L$SHLVL:S$BASH_SUBSHELL\[\e[0m\]'
-# Source file
-PS4+=' \[\e[1;33m\]${BASH_SOURCE:-$0}\[\e[0m\]'
-# Line number
-PS4+='\[\e[0;36m\]#:${LINENO}\[\e[0m\]'
-# Function name
-PS4+='\011\[\e[1;32m\]${FUNCNAME[0]:+${FUNCNAME[0]}(): }\[\e[0m\]'
-# Executed command
-PS4+='\n# '
-export PS4
+intellijView="intellij"
+vscodeView="vscode"
 
-set -eu
+typeset -A nameToCommandMap
+nameToCommandMap[$intellijView]="intellij-idea-ultimate"
+nameToCommandMap[$vscodeView]="code"
 
-function print-msg() {
-  printf '%b%-20s%b' "${colors[${1:-0}]}" "${@:2}" "${colors[0]}"
+getMapValueOrKey() {
+  local key="$1"
+  local value="${nameToCommandMap[$key]}"
+  if [ -z "$value" ]; then
+    echo "$key"
+  else
+    echo "$value"
+  fi
 }
 
-function random-color-echo() {
-  print-msg $((1 + RANDOM % $((4 - 1)))) "${*:-}"
+function testFunction() {
+  originalCmd=$1
+  #for key in "${!nameToCommandMap[@]}"; do
+    #originalCmd=$(echo "$originalCmd" | sed "s/$key/${nameToCommandMap[$key]}/")
+  #  echo "$key"
+  #done
+
+  for key val in "${(@kv)nameToCommandMap}" ; do
+    echo "Key: $key, Value: $val"
+  done
+
+  (eval $originalCmd >/dev/null 2>&1 &)
+
 }
 
-function msg() {
-  random-color-echo "Hi ${*:-}!"
-}
-
-colors=(
-  "$(tput sgr0)"    # reset
-  "$(tput setaf 1)" # red
-  "$(tput setaf 2)" # green
-  "$(tput setaf 3)" # yellow
-  "$(tput setaf 4)" # blue
-)
-
-for item in {"Bob","Alice"}; do
-  echo "$(
-    {
-      msg "$item"
-      (date '+%s%N')
-    } &
-    wait
-  )"
-done
-
-echo 'Done'
+echo "$intellijView /home/serhii/serhii.home/work/git.work/ticket-service"
+testFunction "$intellijView /home/serhii/serhii.home/work/git.work/ticket-service"
