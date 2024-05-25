cf8d339fd94e4799790639b9de7bbf8604f6e7dd
Serhii.Dudar <sergey.dudar@sweetcode.pro>
2024-05-25 16:35:32 +0300

    bash

---
+++ shell-sandbox/tutpoint/12substitution.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#\\ - backslash
+#\a - alert (BEL)
+#\b - backspace
+#\c - suppress trailing newline
+#\f - form feed
+#\n - new line
+#\r - carriage return
+#\t - horizontal tab
+#\v - vertical tab
+
