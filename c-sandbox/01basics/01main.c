b361a5b6d1d75a83161a465d67c34ae85cd83be5
IUADA144 <serhii.dudar@raiffeisen.ua>
2024-12-30 20:36:52 +0200

    c format

--- c-sandbox/01basics/01main.c
+++ c-sandbox/01basics/01main.c
@@ -2,9 +2,9 @@
 #define PI 3.14159
 
 int main() {
-  float radius = 5.0;
-  float area = PI * radius * radius;
+	float radius = 5.0;
+	float area = PI * radius * radius;
 
-  printf("Area of the circle: %f\n", area);
-  return 0;
+	printf("Area of the circle: %f\n", area);
+	return 0;
 }
