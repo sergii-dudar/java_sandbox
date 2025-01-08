4dfcbc7a1996ae4868df4fe8f1f8e6b9f62deed0
IUADA144 <serhii.dudar@raiffeisen.ua>
2025-01-08 12:48:34 +0200

    haskell templates

---
+++ haskell-sandbox/01basics/11template.hs
@@ -0,0 +1,23 @@
+main = do
+    example1
+    example2
+    example3
+    example4
+    example5
+    example6
+    example7
+
+example1 = do
+    putStrLn "\n"
+example2 = do
+    putStrLn "\n"
+example3 = do
+    putStrLn "\n"
+example4 = do
+    putStrLn "\n"
+example5 = do
+    putStrLn "\n"
+example6 = do
+    putStrLn "\n"
+example7 = do
+    putStrLn "\n"
