dbc80558e027a1951b5d1ffbc73b756ba2e44b53
serhii.dudar <shtanga.net@gmail.com>
2025-03-02 21:01:46 +0200

    cpp

---
+++ cpp-sandbox/tests01.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout << sizeof( int ) << '\n'; // print how many bytes of memory an int value takes
+    std::cout << sizeof( long ) << '\n';
+    std::cout << sizeof( double ) << '\n';
+    return 0;
+}
