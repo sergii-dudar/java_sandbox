7a8e949ed1a94bef08a542861f07e5b851facf75
IUADA144 <serhii.dudar@raiffeisen.ua>
2025-10-24 14:40:41 +0300

    changes

--- java-sandbox/maven/serhii-application/src/main/java/ua/serhii/application/Main.java
+++ java-sandbox/maven/serhii-application/src/main/java/ua/serhii/application/Main.java
@@ -1,6 +1,7 @@
 package ua.serhii.application;
 
 import org.apache.commons.collections4.CollectionUtils;
+import ua.serhii.application.com1.TestClass;
 
 import java.util.List;
 
@@ -8,5 +9,6 @@ public class Main {
     public static void main(String[] args) {
         List<String> list = List.of("");
         System.out.println("Hello, World from Maven project! " + CollectionUtils.isEmpty(list));
+        System.out.println("Instance: " + TestClass.INSTANCE);
     }
 }
